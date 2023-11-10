96c925104651f28d0e4fd8193c93e031dcb9ebd0
renevoorburg <voorburg@xs4all.nl>
2023-11-10 19:10:34 +0100

    working on getnames

--- src/Tabula.php
+++ src/Tabula.php
@@ -4,17 +4,18 @@ namespace OmnesViae;
 
 class Tabula
 {
-    private array $data;
+    public array $data;
     private array $routeNetwork;
 
     public function __construct()
     {
         $this->data = json_decode(file_get_contents('../public/data/omnesviae.json'), true);
+
         foreach ($this->data['@graph'] as $value) {
             if ($value['@type'] === 'TravelAction') {
                 if (preg_match('@^.*#(.*)_(.*)$@', $value['@id'], $matches)) {
-                    $this->routeNetwork[$matches[1]][$matches[2]] = (int) $value['distance'];
-                    $this->routeNetwork[$matches[2]][$matches[1]] = (int) $value['distance'];
+                    $this->routeNetwork[$matches[1]][$matches[2]] = (int) $value['dist'];
+                    $this->routeNetwork[$matches[2]][$matches[1]] = (int) $value['dist'];
                 }
             }
         }
--- src/controller.php
+++ src/controller.php
@@ -2,9 +2,60 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+
 $data = new OmnesViae\Tabula();
 
+//$route = new OmnesViae\Route($data->getRouteNetwork(), 'TPPlace558', 'TPPlace1203');
+//$route->getResults();
 
-$route = new OmnesViae\Route($data->getRouteNetwork(), 'TPPlace558', 'TPPlace1203');
-$route->getResults();
+function normalize(string $name) : string
+{
+    $name = strtolower($name);
+    $name = mb_ereg_replace("'", '', $name);
+    $name = mb_ereg_replace('u', 'v', $name);
+    $name = mb_ereg_replace('j', 'i', $name);
+    $name = mb_ereg_replace('v̄', 'v', $name);
 
+    return $name;
+}
+
+
+foreach ($data->data['@graph'] as $value) {
+    if ($value['@type'] === 'Place') {
+        if (!empty($value['label'])) {
+            $arr[normalize($value['label'])] = array('@id' => $value['@id'], 'display' => $value['label']);
+        }
+        if (!empty($value['classic'])) {
+            $arr[normalize($value['classic'])] = array('@id' => $value['@id'], 'display' => $value['classic']);
+        }
+        if (!empty($value['modern'])) {
+            $arr[normalize($value['modern'])] = array('@id' => $value['@id'], 'display' => $value['modern']);
+        }
+        if (!empty($value['alt'])) {
+            $arr[normalize($value['alt'])] = array('@id' => $value['@id'], 'display' => $value['alt']);
+        }
+
+    }
+}
+
+ksort($arr);
+//print_r($arr);
+$chars="aba";
+
+$filteredArray = array();
+$found = false;
+foreach ($arr as $key => $value) {
+    if (strpos($key, $chars) === 0) {
+        // Key starts with the given sequence of characters
+        $filteredArray[$key] = $value;
+        $found = true;
+    } elseif ($found === true) { break; }
+}
+
+echo "[";
+$sep = "";
+foreach ($filteredArray as $key => $value) {
+    echo $sep . '{ "label": "' . $value['display'] . ', "value": "' . $value['@id'] . '" }';
+    $sep = ",";
+}
+echo "]";
